2341234048ad535c03e5849f158454e61f2743f0
BrianC <whguddms8647@naver.com>
2023-04-11 00:28:20 +0900

    기존 파일 수정 과정에서 계속해서 충돌하여 리베이스 과정을 거쳐서 재수정 업로드

---
+++ src/test/java/com/rainbowsweet/lastdance/LastdanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowsweet.lastdance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastdanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
